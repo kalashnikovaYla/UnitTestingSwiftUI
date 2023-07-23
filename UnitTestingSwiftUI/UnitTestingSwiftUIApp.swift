31dc8896bbd9b805705b2f606d20d9a36047004f
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-07-23 13:01:22 +0500

    Initial Commit

---
+++ UnitTestingSwiftUI/UnitTestingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestingSwiftUIApp.swift
+//  UnitTestingSwiftUI
+//
+//  Created by sss on 23.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
